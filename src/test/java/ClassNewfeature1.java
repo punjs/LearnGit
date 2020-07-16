5abfb6fe5553e20aec87bdde4ff3dfe920a080e6
punjs <sandeep.punj@gmail.com>
2020-07-16 23:05:45 +0530

    Commited ClassNewfeature1

---
+++ src/test/java/ClassNewfeature1.java
@@ -0,0 +1,13 @@
+
+public class ClassNewfeature1
+{
+
+    static String feature1;
+
+    public static void main(String[] args)
+    {
+        System.out.println("The feature developed in this branch is String   " + feature1);
+
+    }
+
+}
