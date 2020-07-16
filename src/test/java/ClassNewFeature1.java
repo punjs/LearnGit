13f88a1460bf7c2e1d2b6c7f437bc850ad3bbcab
punjs <sandeep.punj@gmail.com>
2020-07-16 22:42:28 +0530

    First commit to Feature Branch

---
+++ src/test/java/ClassNewFeature1.java
@@ -0,0 +1,13 @@
+
+public class ClassNewFeature1
+{
+
+    static String feature;
+
+    public static void main(String[] args)
+    {
+        System.out.println("The feature developed in this branch is String   " + feature);
+
+    }
+
+}
---
+++ src/test/java/ClassNewFeature2.java
@@ -0,0 +1,13 @@
+
+public class ClassNewFeature2 {
+
+	
+		static String feature2;
+		public static void main(String[] args) {
+		System.out.println("The feature developed in this branch is String   "+feature2);
+
+		
+
+	}
+	
+}
